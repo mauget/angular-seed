95550663de7f09a8a96e2f3d9607ba4e016a3a19
L.E. Mauget <mauget@gmail.com>
2013-09-07 13:23:07 -0400

    Updated karma.conf.js to work with node v0.10.19 and karama v0.10.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,31 +1,72 @@
-module.exports = function(config){
-    config.set({
-    basePath : '../',
+// Karma configuration
+// Generated on Sat Sep 07 2013 11:33:40 GMT-0400 (EDT)
 
-    files : [
-      'app/lib/angular/angular.js',
-      'app/lib/angular/angular-*.js',
-      'test/lib/angular/angular-mocks.js',
-      'app/js/**/*.js',
-      'test/unit/**/*.js'
-    ],
+module.exports = function(config) {
+  config.set({
 
-    autoWatch : true,
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
+
+    // frameworks to use
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
 
-    plugins : [
-            'karma-junit-reporter',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine'       
-            ],
+    // list of files / patterns to load in the browser
+    files: [
+      
+    ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
 
-})}
+    // list of files to exclude
+    exclude: [
+   	'app/lib/angular/angular.js',
+        'app/lib/angular/angular-*.js',
+        'test/lib/angular/angular-mocks.js',
+        'app/js/*.js',
+        'test/unit/*.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
